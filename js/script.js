0919365ed333a1d77b75346d320d49d3b949965a
kontilena <64475765+kontilena@users.noreply.github.com>
2022-02-21 18:58:39 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
-let goodsList = document.querySelector('.goods__list');
-let nextButton = document.querySelector('.slider__button--next');
-let prevButton = document.querySelector('.slider__button--prev'); 
-let move = 0;
-nextButton.addEventListener('click', function(){
-    if (move <= -3540) {
-        move = 0 ;
-    } else {
-        move -= 1180;
+let slider = document.querySelector('.slider__list');
+let rBtn = document.querySelector('.slider__button--next');
+let lBtn = document.querySelector('.slider__button--prev'); 
+
+rBtn.addEventListener('click', function() {
+    for(var i=0;i<5;i++){
+        slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
     }
-    goodsList.style = "transform: translateX(" + move +"px)";
+
 });
-prevButton.addEventListener('click', function(){
-    if (move >= 0) {
-        move = -3540 ;
-    } else {
-        move += 1180;
+lBtn.addEventListener('click', function() {
+    for(var i=0;i<5;i++){
+        slider.insertBefore(slider.firstElementChild, slider.lastElementChild);
     }
-    goodsList.style = "transform: translateX(" + move +"px)";
+    
 });
